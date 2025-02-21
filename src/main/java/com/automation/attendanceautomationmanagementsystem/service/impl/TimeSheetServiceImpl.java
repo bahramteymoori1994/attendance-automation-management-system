ed65c0c0109b914987932bfcdb750a133aeb0a56
bahramteymoori <bahram.teymoori@outlook.com>
2025-02-21 19:47:30 +0330

    TimeSheetServiceImpl bug fixed

--- src/main/java/com/automation/attendanceautomationmanagementsystem/service/impl/TimeSheetServiceImpl.java
+++ src/main/java/com/automation/attendanceautomationmanagementsystem/service/impl/TimeSheetServiceImpl.java
@@ -2,11 +2,10 @@ package com.automation.attendanceautomationmanagementsystem.service.impl;
 
 import com.automation.attendanceautomationmanagementsystem.model.TimeSheet;
 import com.automation.attendanceautomationmanagementsystem.repository.TimeSheetRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TimeSheetServiceImpl extends AbstractBaseServiceImpl<TimeSheet, TimeSheetRepository> implements JpaRepository<TimeSheet, Long> {
+public class TimeSheetServiceImpl extends AbstractBaseServiceImpl<TimeSheet, TimeSheetRepository>{
     public TimeSheetServiceImpl(TimeSheetRepository repository) {
         super(repository);
     }
