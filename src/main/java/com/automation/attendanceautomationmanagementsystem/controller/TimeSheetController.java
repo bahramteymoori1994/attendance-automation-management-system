98e545f709b32d587d93b6241464b20a9bd1a284
bahramteymoori <bahram.teymoori@outlook.com>
2025-02-21 20:23:34 +0330

    bug fixed

--- src/main/java/com/automation/attendanceautomationmanagementsystem/controller/TimeSheetController.java
+++ src/main/java/com/automation/attendanceautomationmanagementsystem/controller/TimeSheetController.java
@@ -1,13 +1,12 @@
 package com.automation.attendanceautomationmanagementsystem.controller;
 
 import com.automation.attendanceautomationmanagementsystem.model.TimeSheet;
-import com.automation.attendanceautomationmanagementsystem.service.impl.AbstractBaseServiceImpl;
 import com.automation.attendanceautomationmanagementsystem.service.impl.TimeSheetServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/time_sheet")
-public class TimeSheetController extends AbstractBaseServiceImpl<TimeSheet, TimeSheetServiceImpl>
+public class TimeSheetController extends AbstractBaseController<TimeSheet, TimeSheetServiceImpl>
 {
     public TimeSheetController(TimeSheetServiceImpl repository) {
         super(repository);
